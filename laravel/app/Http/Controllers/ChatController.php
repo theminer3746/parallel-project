64f8582a6c636fd3464b3e1584b2a6687be34f18
theminer3746 <me@theminerdev.com>
2020-04-30 22:11:33 +0700

    Edit ChatController

---
+++ laravel/app/Http/Controllers/ChatController.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Chat;
+use App\User;
+use App\Message;
+use Exception;
+
+class ChatController extends Controller
+{
+    private $chat;
+
+    /**
+     * Create a new ChatController instance.
+     *
+     * @return void
+     */
+    public function __construct(Chat $chat)
+    {
+        $this->middleware('auth:api');
+    
+        $this->chat = $chat;
+    }
+
+    public function create(Request $request)
+    {
+        try{
+            $chatId = $this->chat->createNewChat($request->name);
+            $this->chat->addUserToChat($chatId, auth()->payload()->get('sub'));
+            User::find(auth()->payload()->get('sub'))->addUserToChat($chatId, auth()->payload()->get('sub'));
+        }catch(Exception $e){
+            return response()->json([
+                'message' => 'Internal Server Error',
+            ], 500);
+        }
+
+        return response()->json([], 201);
+    }
+
+    public function join(Request $request)
+    {
+        $this->chat->addUserToChatByInviteCode($request->invite_code, auth()->payload()->get('sub'));
+
+        return response()->json();
+    }
+
+    public function newMessage(Request $request, $chatId, Message $message)
+    {
+        $request->validate([
+            'message' => 'required',
+            'user_id' => 'required',
+        ]);
+
+        $messageDto = new \App\MessageDto($request->message, $request->user_id);
+
+        $message->newMessage($chatId, $messageDto);
+
+        return response()->json([], 201);
+    }
+}
