e93f86a5b5111062c3d8272fa2eb8f5d378fad79
theminer3746 <me@theminerdev.com>
2020-05-01 17:48:24 +0700

    Create database connection configuration to mongodb replica

--- laravel/config/database.php
+++ laravel/config/database.php
@@ -105,6 +105,22 @@ return [
                 'database' => env('DB_AUTHENTICATION_DATABASE', 'parallel'), // required with Mongo 3+
             ],
         ],
+
+        'mongodb_replication' => [
+            'driver' => 'mongodb',
+            'host' => explode(',', env('DB_HOST', '127.0.0.1')),
+            'port' => env('DB_PORT', 27017),
+            'database' => env('DB_DATABASE', 'homestead'),
+            'username' => env('DB_USERNAME', 'homestead'),
+            'password' => env('DB_PASSWORD', 'secret'),
+            'options' => [
+                // here you can pass more settings to the Mongo Driver Manager
+                // see https://www.php.net/manual/en/mongodb-driver-manager.construct.php under "Uri Options" for a list of complete parameters that you can use
+
+                'replicaSet' => 'rs0',
+                'database' => env('DB_AUTHENTICATION_DATABASE', 'parallel'), // required with Mongo 3+
+            ],
+        ],
     ],
 
     /*
